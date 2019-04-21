ecb179ae0b172788d0281c4931732bac6c54dc3a
edward.kats.course <edward.kats.course@gmail.com>
2019-04-21 20:37:13 +0300

    first commit

---
+++ src/main/java/com/example/demoultimate/DemoUltimateApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUltimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoUltimateApplication.class, args);
+    }
+}
